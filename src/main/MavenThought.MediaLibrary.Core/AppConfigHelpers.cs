f13e6114cecf1ff2c77fd25a0534ca95e28a55b5
David Alpert <david@spinthemoose.com>
2010-03-28 21:48:35 -0500

    added initial cut of AppConfigHelper + tests, imported from Bundl.It source codeand cleaned up a bit;

---
+++ src/main/MavenThought.MediaLibrary.Core/AppConfigHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+
+namespace MavenThought.MediaLibrary.Core
+{
+    public static class AppConfigHelpers
+    {
+        public static string STR_CannotFindConnectionFormatString = "Cannot find connection string: {0}";
+        public static string STR_MachineSpecificFormatString = "{1}-{0}";
+
+        public static string ToMachineSpecificConfigKey(this string abstractConfigKeyName)
+        {
+            return String.Format(STR_MachineSpecificFormatString, Environment.MachineName, abstractConfigKeyName);
+        }
+
+        public static string ToMachineSpecificConnectionString(this string abstractConnectionStringName)
+        {
+            var concreteConnectionStringName = abstractConnectionStringName.ToMachineSpecificConfigKey();
+
+            if (ConfigurationManager.ConnectionStrings[concreteConnectionStringName] == null)
+            {
+                throw new InvalidOperationException(String.Format(STR_CannotFindConnectionFormatString, concreteConnectionStringName));
+            }
+
+            return ConfigurationManager.ConnectionStrings[concreteConnectionStringName].ConnectionString;
+        }
+    }
+}
---
+++ src/test/MavenThought.MediaLibrary.Core.Tests/AppConfigHelperTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SharpTestsEx;
+using MbUnit.Framework;
+using MavenThought.MediaLibrary.Core;
+
+namespace MavenThought.MediaLibrary.Core.Tests
+{
+    [TestFixture]
+    public class AppConfigHelperTests
+    {
+        [Test]
+        public void can_resolve_abstract_config_key_name_to_machine_specific_config_key_name()
+        {
+            string STR_ExpectedFormatString = "{1}-{0}";
+            string expectedKeyName = String.Format(STR_ExpectedFormatString, Environment.MachineName, "TestConnection");
+
+            "TestConnection".ToMachineSpecificConfigKey().Should().Be.EqualTo(expectedKeyName);
+        }
+
+        [Test]
+        public void resolving_a_nonexistant_connection_string_throws()
+        {
+            var expectedConnectionStringName = "nonexistantConnectionString".ToMachineSpecificConfigKey();
+            var expectedErrorMessage = String.Format(AppConfigHelpers.STR_CannotFindConnectionFormatString, expectedConnectionStringName);
+
+            var ex = Assert.Throws<InvalidOperationException>(() =>
+            {
+                "nonexistantConnectionString".ToMachineSpecificConnectionString();
+            });
+
+            ex.Message.Should().Be.EqualTo(expectedErrorMessage);
+        }
+    }
+}
